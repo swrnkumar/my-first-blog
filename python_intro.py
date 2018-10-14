78802e1a45f3e2a520527fbabbd0c090c36b6231
Swarna <swrnkumar@yahoo.com>
2018-10-14 14:10:21 +0200

    My Django girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,13 @@
+def hi(name):
+	print ('Hi ' + name + '!')
+
+
+girls = ['Swa', 'Nam', 'Ann', 'Monica']
+for name in girls:
+	hi(name)
+	print('next')
+
+
+
+for i in range(2,10):
+	print (i)
---
+++ test.py
@@ -0,0 +1,5 @@
+import datetime
+
+a = datetime.datetime.now()
+
+print(str(a), "\n")
